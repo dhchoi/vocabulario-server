d9759f40762e13b02900642c76011830a2f9b9f4
Dong Hyun Choi <andy.dh.choi@gmail.com>
2015-07-26 18:02:26 -0500

    add toasters and fix bug where newly added cards can't get deleted

--- public/js/words-main.js
+++ public/js/words-main.js
@@ -7,24 +7,11 @@ $(window).load(function () {
         transitionDuration: 0
     });
 
-    $(".btn-delete-word").on("click", function () {
-        var $wordCardGridItem = $(this).parent().parent().parent();
-        $.ajax({
-            url: "/api/web/delete",
-            method: "POST",
-            data: "word=" + $(this).data().word
-        }).done(function(response) {
-            if (response.result) {
-                $grid.masonry("remove", $wordCardGridItem).masonry("layout");
-            }
-        });
-    });
-
+    $(".btn-delete-word").on("click", deleteWord);
     $(".btn-add-word").on("click", function(event) {
         event.preventDefault();
         addWord();
     });
-
     $(".form-add-word").on("keypress", function(event){
         if (event.which == 13) {
             event.preventDefault();
@@ -40,7 +27,29 @@ $(window).load(function () {
         }).done(function (response) {
             if (response.result) {
                 var $newWordCard = $(Handlebars.compile($("#word-card-template").html())(response));
+                $newWordCard.find(".btn-delete-word").on("click", deleteWord);
                 $grid.append($newWordCard).masonry("appended", $newWordCard);
+                toastr.success(response.message);
+            }
+            else {
+                toastr.warning(response.message);
+            }
+        });
+    }
+
+    function deleteWord() {
+        var $wordCardGridItem = $(this).parent().parent().parent();
+        $.ajax({
+            url: "/api/web/delete",
+            method: "POST",
+            data: "word=" + $(this).data().word
+        }).done(function(response) {
+            if (response.result) {
+                $grid.masonry("remove", $wordCardGridItem).masonry("layout");
+                toastr.success(response.message);
+            }
+            else {
+                toastr.warning(response.message);
             }
         });
     }
