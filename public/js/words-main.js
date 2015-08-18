d15ce013b3b0b9e29c879b05548f2edde6ecbe18
Dong Hyun Choi <andy.dh.choi@gmail.com>
2015-08-18 01:20:40 -0500

    add favorite btn and revamp styles

--- public/js/words-main.js
+++ public/js/words-main.js
@@ -1,79 +1,109 @@
 $(window).load(function () {
-    var $btnAddWord = $(".btn-add-word");
+  var $btnAddWord = $(".btn-add-word");
 
-    initializeBarRating($('.select-confidence-rate'));
+  initializeBarRating($('.select-confidence-rate'));
 
-    var $grid = $('.word-card-grid');
-    $grid.masonry({
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true,
-        transitionDuration: 0
-    });
+  var $grid = $('.word-card-grid');
+  $grid.masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    transitionDuration: 0
+  });
 
-    toastr.options = {
-        "positionClass": "toast-bottom-right",
-        "progressBar": true
-    };
-
-    $(".btn-delete-word").on("click", deleteWord);
-    $btnAddWord.on("click", function(event) {
-        event.preventDefault();
-        addWord();
-    });
-    $(".form-add-word").on("keypress", function(event){
-        if (event.which == 13) {
-            event.preventDefault();
-            addWord();
-        }
-    });
-
-    function addWord() {
-        $btnAddWord.button("loading");
-        $.ajax({
-            url: "/api/web/add",
-            method: "POST",
-            data: "word="+$(".input-add-word").val()
-        }).done(function (response) {
-            if (response.result) {
-                var $newWordCard = $(Handlebars.compile($("#word-card-template").html())(response));
-                $newWordCard.find(".btn-delete-word").on("click", deleteWord);
-                initializeBarRating($newWordCard.find(".select-confidence-rate"));
-                $grid.append($newWordCard).masonry("appended", $newWordCard);
-                toastr.success(response.message);
-            }
-            else {
-                toastr.warning(response.message);
-            }
-            $btnAddWord.button("reset");
-        });
+  $(".card-control").on("click", function () {
+    var $this = $(this);
+    var $i = $this.find("i");
+    if($i.hasClass("fa-bars")) {
+      $i.removeClass("fa-bars");
+      $i.addClass("fa-times");
     }
-
-    function deleteWord() {
-        var $wordCardGridItem = $(this).closest(".grid-item");
-        $.ajax({
-            url: "/api/web/delete",
-            method: "POST",
-            data: "word=" + $(this).data().word
-        }).done(function(response) {
-            if (response.result) {
-                $grid.masonry("remove", $wordCardGridItem).masonry("layout");
-                toastr.success(response.message);
-            }
-            else {
-                toastr.warning(response.message);
-            }
-        });
+    else {
+      $i.addClass("fa-bars");
+      $i.removeClass("fa-times");
     }
+    var $cardActions = $this.parent().find(".card-actions");
+    $cardActions.slideToggle({
+      duration: "fast",
+      progress: function () {
+        $grid.masonry('layout');
+      }
+    });
+  });
 
-    function initializeBarRating($el) {
-        $el.barrating({
-            theme: 'bars-1to10',
-            showValues: false,
-            showSelectedRating: false,
-            onSelect: function () {
-                console.log("selected!");
-            }
-        });
+  toastr.options = {
+    "positionClass": "toast-bottom-right",
+    "progressBar": true
+  };
+
+  $(".btn-star-word").on("click", starWord);
+  $(".btn-confirm-rate").on("click", confirmRate);
+  $(".btn-delete-word").on("click", deleteWord);
+  $btnAddWord.on("click", function (event) {
+    event.preventDefault();
+    addWord();
+  });
+  $(".form-add-word").on("keypress", function (event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addWord();
     }
+  });
+
+  function addWord() {
+    $btnAddWord.button("loading");
+    $.ajax({
+      url: "/api/web/add",
+      method: "POST",
+      data: "word=" + $(".input-add-word").val()
+    }).done(function (response) {
+      if (response.result) {
+        var $newWordCard = $(Handlebars.compile($("#word-card-template").html())(response));
+        $newWordCard.find(".btn-delete-word").on("click", deleteWord);
+        initializeBarRating($newWordCard.find(".select-confidence-rate"));
+        $grid.append($newWordCard).masonry("appended", $newWordCard);
+        toastr.success(response.message);
+      }
+      else {
+        toastr.warning(response.message);
+      }
+      $btnAddWord.button("reset");
+    });
+  }
+
+  function deleteWord() {
+    var $wordCardGridItem = $(this).closest(".grid-item");
+    $.ajax({
+      url: "/api/web/delete",
+      method: "POST",
+      data: "word=" + $(this).data().word
+    }).done(function (response) {
+      if (response.result) {
+        $grid.masonry("remove", $wordCardGridItem).masonry("layout");
+        toastr.success(response.message);
+      }
+      else {
+        toastr.warning(response.message);
+      }
+    });
+  }
+
+  function initializeBarRating($el) {
+    $el.barrating({
+      theme: 'bars-1to10',
+      showValues: false,
+      showSelectedRating: false,
+      onSelect: function () {
+        $(this).closest(".card-actions").find(".btn-confirm-rate").show();
+      }
+    });
+  }
+
+  function starWord() {
+    $(this).find("i").toggleClass("starred");
+  }
+
+  function confirmRate() {
+    $(this).hide();
+  }
 });
